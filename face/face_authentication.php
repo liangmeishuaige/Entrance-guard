c5948fc607b5e88404d21ae8e44c3c2b6f6f0004
HenryShen <1165106152@qq.com>
2018-11-23 15:24:42 +0800

    aaa

--- face/face_authentication.php
+++ face/face_authentication.php
@@ -1,7 +1,8 @@
 <?php
 
 	include 'AipFace.php';
-	
+	//连接数据库代码块
+	include 'lianjie.php';
 	require_once 'AipFace.php';
 
 	// 你的 APPID AK SK
@@ -46,12 +47,64 @@
 	
 	if($score>=80){
 		echo 1;
+		//现在是Henry改写的
+		//功能：
+		//		记录此时验证拍下的照片，以时间戳为文件名，传入数据库
+		//
+		$file = $_FILES["img"];
+		// 先判断有没有错
+		if ($file["error"] == 0) {
+			// 成功 
+			// 判断传输的文件是否是图片，类型是否合适
+			// 获取传输的文件类型
+			$typeArr = explode("/", $file["type"]);
+			if($typeArr[0]== "image")
+			{
+				// 如果是图片类型
+				$imgType = array("png","jpg","jpeg");
+				if(in_array($typeArr[1], $imgType)){ // 图片格式是数组中的一个
+					// 类型检查无误，保存到文件夹内
+					// 给图片定一个新名字 (使用时间戳，防止重复)
+					$fname = date("Y-m-d-H-i-s-A",time());//时间做文件名,年月日+具体时间
+					$ext = strrchr($file['name'],'.');//截取文件的扩展名
+					$path = './'.date('Y/m/d');//文件存储的路径
+					//如果目录不存在，就创建目录
+					if(!is_dir($path))
+					{
+						mkdir($path,0777,true);
+					}
+					$saveFile = $path.'/'.$fname.$ext;//保存的文件名
+					// 将上传的文件写入到文件夹中
+					// 参数1: 图片在服务器缓存的地址
+					// 参数2: 图片的目的地址（最终保存的位置）
+					// 最终会有一个布尔返回值
+					//echo move_uploaded_file($file["tmp_name"],$saveFile)?"ok":"fail";
+					
+					move_uploaded_file($file["tmp_name"],$saveFile);
+				};
+				else
+				{
+					// 不是图片类型
+					echo "没有图片，再检查一下吧！";
+				};
+		}
+		else
+		{
+			// 失败
+			echo $file["error"];
+		}
+		//
+		//
 		
 		// 调用人脸更新
 		//$result = $client->updateUser($image, $imageType, $groupId, $userId);
 		
 		//var_dump($result);
 	}else{
+		//现在是Henry改写的
+		//功能：
+		//		记录此时验证拍下的照片，以时间戳为文件名，传入数据库
+		//
 		echo 0;
 	}
 
