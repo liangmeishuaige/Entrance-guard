18ab983fa32123995a3242d070d9de23f64db736
summer_sea <xiahaohai@qq.com>
2018-11-30 21:43:15 +0800

    更新~

---
+++ face/face_authentication.php
@@ -0,0 +1,68 @@
+<?php
+
+	//Disable error reporting
+	error_reporting(0);
+
+	include 'AipFace.php';
+	
+	require_once 'AipFace.php';
+
+	// 你的 APPID AK SK
+	const APP_ID = '14511390';
+	const API_KEY = 'LSfn1X7S0FqfDq1GvxTGloVe';
+	const SECRET_KEY = 'NkPvLLkO3EmDmI7VsMXloYKCZUhbZ9VY';
+
+	$client = new AipFace(APP_ID, API_KEY, SECRET_KEY);
+	//var_dump($_POST);
+
+	//$groupId = "001";//组ID
+	
+	$image = $_POST["base64_data"];
+	
+	//var_dump($image);	
+	/*$encode = '<img src="data:image/jpg/png/gif;base64,' . $image .'" >'; 
+			    echo $encode; */
+
+	$imageType = "BASE64";
+
+	$groupIdList = "resident,tourist,stranger";
+	
+	/*$userId = $_POST["user_id"];//用户ID
+	
+	$options = array();
+	$options["quality_control"] = "NORMAL";
+	$options["liveness_control"] = "LOW";
+	$options["user_id"] = "$userId";//用户ID
+	$options["max_user_num"] = 1;*/
+
+	// 调用人脸搜索
+	
+
+	$result = $client->search($image, $imageType, $groupIdList);
+	
+	$result2 = $result["result"]["user_list"]["0"];
+	
+	$group_id = $result2["group_id"];
+	$user_id = $result2["user_id"];
+	$score = $result2["score"];
+	
+	$facetoken = $result["result"]["face_token"];
+	
+	//var_dump($result);
+	//var_dump($result2);
+	
+	//echo "\r\n";
+	
+	if($score>=80){
+		echo "Welcome! ".$group_id." : ".$user_id;
+		
+		
+		// 调用人脸更新
+		//$result = $client->updateUser($image, $imageType, $groupId, $userId);
+		
+		//var_dump($result);
+	}else{
+		echo "stranger";
+	}
+
+?>
