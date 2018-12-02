42fac6edb3e00bd6629f9c7cb3e301c48c9cfd3d
summer_sea <xiahaohai@qq.com>
2018-12-02 14:21:37 +0800

    修改后的人脸注册代码,要求先正脸后侧脸，三张照片， 目前是居民注册用的，你们可以按需求把groupid改成tourist，就会成为访客用

--- face_authentication/face_register.php
+++ face_authentication/face_register.php
@@ -1,5 +1,7 @@
 <?php
 
+	header('Content-Type:text/html;charset=GB2312');
+	
 	include 'AipFace.php';
 	
 	require_once 'AipFace.php';
@@ -30,21 +32,89 @@
 		$client->deleteUser($groupId,$userId);
 	}
 	
+	$ECheck=-1;
 	
-	// 调用人脸注册
-	$result = $client->addUser($image, $imageType, $groupId, $userId);
-	$error_code = $result["error_code"];
-	if($error_code=="0"){
-		if($sign=="0"){
-			echo("progress:1/3");
-		}elseif($sign=="1"){
-			echo("progress:2/3");
+	$detectResult=$client->detect($image, $imageType);
+	$detectErrorCode=$detectResult["error_code"];
+	if($detectErrorCode==0){
+		$angle = $detectResult["result"]["face_list"]["0"]["angle"];
+	
+		$face_token=$detectResult["result"]["face_list"]["0"]["face_token"];
+		
+		$yaw=$angle["yaw"];
+		$pitch=$angle["pitch"];
+		
+		if($pitch>-15&&$pitch<25){
+			$pitchCheck=1;
 		}else{
-			echo("progress:3/3");
+			$pitchCheck=-3;
 		}
-	}else{
-		echo("progress:0/3");
+		//echo($pitchCheck);
+		
+		if($yaw>-15&&$yaw<15){
+			$yawCheck=2;
+		}elseif($yaw>=15&&$yaw<55){
+			$yawCheck=1;
+		}elseif($yaw>-55&&$yaw<=-15){
+			$yawCheck=0;
+		}
+		//echo($yawCheck);
+		
+		/*$sign0="0";
+		$sign1="1";
+		$sign2="2";
+		
+		$ECheck=(int)$sign0+$yawCheck+$pitchCheck;
+		echo($ECheck);
+		$ECheck=(int)$sign1+$yawCheck+$pitchCheck;
+		echo($ECheck);
+		$ECheck=(int)$sign2+$yawCheck+$pitchCheck;
+		echo($ECheck);*/
+		
+		$ECheck=(int)$sign+$yawCheck+$pitchCheck;
 	}
+
+	
+	
+	
+	
+	if($detectErrorCode==0&&$ECheck==3){
+		// 调用人脸注册
+		
+		$image=$face_token;
+		$imageType="FACE_TOKEN";
+		
+		$result = $client->addUser($image, $imageType, $groupId, $userId);
+		$error_code = $result["error_code"];
+		
+		if($error_code=="0"){
+			if($sign=="0"){
+				echo("注册进度:1/3 请用左侧脸对着屏幕");
+			}elseif($sign=="1"){
+				echo("注册进度:2/3 请用右侧脸对着屏幕");
+			}else{
+				echo("注册进度:3/3 注册完成，即将跳转。");
+			}
+		}else{
+			if($sign=="0"){
+				echo("注册进度:0/3 请用正脸对着屏幕");
+			}elseif($sign=="1"){
+				echo("注册进度:1/3 请用左侧脸对着屏幕");
+			}else{
+				echo("注册进度:2/3 请用右侧脸对着屏幕");
+			}
+		}
+	}else{			//不调用人脸注册对策
+		if($sign=="0"){
+			echo("注册进度:0/3 请用正脸对着屏幕");
+		}elseif($sign=="1"){
+			echo("注册进度:1/3 请用左侧脸对着屏幕");
+		}else{
+			echo("注册进度:2/3 请用右侧脸对着屏幕");
+		}
+	}
+	
+	
 	
 	//echo $error_code;
 	//var_dump($result);
